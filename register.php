c8718f9a335e93b0ae9265c224b9917784af7be6
chelovecheki <36590480+chelovecheki@users.noreply.github.com>
2019-07-02 17:51:09 +0700

    Добавлены стили

--- register.php
+++ register.php
@@ -3,6 +3,6 @@
 require_once __DIR__.'/init.php';
 require_once 'Auth_class.php';
 
-Authentication::Login();
+Authentication::Register();
 
-echo $twig->render('index.html');
+echo $twig->render('register.html');
